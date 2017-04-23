348d1de2f440a4bd4784cb4b756c9e714c2acafe
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2017-04-23 14:55:56 +0300

    Optimaze running tests in parallel

--- tests/serenity/src/test/java/com/tkmdpa/taf/AcceptanceTestSuite.java
+++ tests/serenity/src/test/java/com/tkmdpa/taf/AcceptanceTestSuite.java
@@ -2,6 +2,11 @@ package com.tkmdpa.taf;
 
 import net.serenitybdd.jbehave.SerenityStories;
 
-public class AcceptanceTestSuite extends SerenityStories {
+public class AcceptanceTestSuite extends OptimizedParallelAcceptanceTestSuite {
+
+    public AcceptanceTestSuite() {
+        selectStoryFilesForRunningSuite();
+    }
+
     public static String baseTestedUrl = new SerenityStories().getEnvironmentVariables().getProperty("webdriver.base.url");
 }
