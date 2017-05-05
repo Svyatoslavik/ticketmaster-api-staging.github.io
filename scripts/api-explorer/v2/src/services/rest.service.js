a8bb136cba2bfcc40cdc84ef2f4d5661f6eafc3b
Sviatoslav Mysachenko <Sviatoslav_Mysachenko@epam.com>
2017-05-05 12:30:50 +0300

    up

--- scripts/api-explorer/v2/src/services/rest.service.js
+++ scripts/api-explorer/v2/src/services/rest.service.js
@@ -15,7 +15,7 @@ class RestService {
 			instance = this;
 		}
 		this.base = base;
-		this.base.some = 1;
+		this.base.some = 2;
 		this.apiKey = apikey;
 		this.selectedCategory = ko.observable('').subscribeTo('SELECTED_CATEGORY');
 		this.selectedMethodType = ko.observable('').subscribeTo('SELECTED_METHOD_TYPE');
